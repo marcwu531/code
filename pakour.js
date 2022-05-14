e85b777fd530cbca806d5031058376b805fd0bc7
marcwu531@gmail.com <marcwu531@gmail.com>
2022-05-15 00:02:45 +0800

    fixed bug

--- pakour.js
+++ pakour.js
@@ -1,11 +1,11 @@
 document.getElementById("pakoursetting").style.display = "none";
 
 function pakour() {
-    document.getElementById("pakoursetting").style.display = "block";
+    /*document.getElementById("pakoursetting").style.display = "block";
     var button = document.getElementById("pakour");
     button.style.bottom = "907px"; //(+(button.style.bottom.split("px")[0]) + 30) + "px";
     //let buttonpx = button.style.bottom;
    // buttonpx = buttonpx.split("px");
     //buttonpx = buttonpx[0];
-    
+    */
 }
