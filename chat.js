ae4aeafc6b0747d244fc7b8ef84b9e3cbf5600cb
marcwu531 <49812037+marcwu531@users.noreply.github.com>
2022-05-16 11:55:41 +0800

    test

--- chat.js
+++ chat.js
@@ -1,10 +1,19 @@
-let ans;
-function chat() {
-    ans = prompt("","");
+//let ans;
+/*function chat() {
+    //ans = prompt("","");
     /*if(ans == "HI"){
         alert("Hi");
     }*/
-    //var chatletter = document.getElementById("chatbutton");
-    //chatletter.value = ans;
-    alert("Nedon Cheung is Gay");
+    /*var chatletter = document.getElementById("chatbutton");
+    chatletter.value = ans;
+    alert("Test");
+}*/
+
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
